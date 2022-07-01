f6a770e1c07f88b28679dabe170266f3ee3745a5
phaitoon-sirnil <47589376+phaitoon-sirnil@users.noreply.github.com>
2022-07-01 21:15:46 +0700

    Update ex_mqtt.ino

--- ex_mqtt.ino
+++ ex_mqtt.ino
@@ -2,17 +2,17 @@
 #include <ESP8266WiFi.h>
 #include<PubSubClient.h>
 
-#define time_duration 30000
+#define time_duration 10000
 
-const char *phone = "0612230586";
+const char *phone = "buu-01";
 const char *house = "house1";
 
 //const char *ssid = "Perfact land 2.4G_EXT";//“********”; // cannot be longer than 32 characters!
 //const char *password = "2499265MJ";//“*********”;
-const char *ssid = "TP-Link_6EA2";//“********”; // cannot be longer than 32 characters!
-const char *password = "26122144";//“*********”;
-//const char *ssid = "Redmi_9A";//“********”; // cannot be longer than 32 characters!
-//const char *password = "0813470881";//“*********”;
+//const char *ssid = "TP-Link_6EA2";//“********”; // cannot be longer than 32 characters!
+//const char *password = "26122144";//“*********”;
+const char *ssid = "Redmi_9A";//“********”; // cannot be longer than 32 characters!
+const char *password = "0813470881";//“*********”;
 
 const char *mqtt_server = "broker.hivemq.com";//“m14.cloudmqtt.com”;
 const int mqtt_port = 1883;//19405;
@@ -65,58 +65,62 @@ void callback(String topic,byte* payload,unsigned int length1){
   Serial.print("\n");
   char pub_topic[32];
   
-  if(topic == String("/")+String(phone)+String("/house1/control/spray")){
+  if(topic == String("/")+String(phone)+String("/")+String(house)+String("/control/spray")){
     if(msgg == "ON_SPRAY"){
-      data = String("/")+String(phone)+String("/house1/state/spray");
+      data = String("/")+String(phone)+String("/")+String(house)+String("/state/spray");
       data.toCharArray(pub_topic,data.length()+1);
        digitalWrite(SPRAY,HIGH); 
        client.publish(pub_topic,"ON");  
-       Serial.println("/buu/iot/diy/state/spray,ON");    
+       Serial.print("publish(");Serial.print(pub_topic);Serial.println(",ON)");    
     }else if(msgg == "OFF_SPRAY"){
-      data = String("/")+String(phone)+String("/house1/state/spray");
+      data = String("/")+String(phone)+String("/")+String(house)+String("/state/spray");
       data.toCharArray(pub_topic,data.length()+1);
        digitalWrite(SPRAY,LOW); 
        client.publish(pub_topic,"OFF");   
-       Serial.println("/buu/iot/diy/state/spray,OFF");
+       Serial.print("publish(");Serial.print(pub_topic);Serial.println(",OFF)"); 
     }
-  }else if(topic==String("/")+String(phone)+String("/house1/control/fan")){
+  }else if(topic==String("/")+String(phone)+String("/")+String(house)+String("/control/fan")){
     if(msgg=="ON_FAN"){
-      data = String("/")+String(phone)+String("/house1/state/fan");
+      data = String("/")+String(phone)+String("/")+String(house)+String("/state/fan");
       data.toCharArray(pub_topic,data.length()+1);
       digitalWrite(FAN,HIGH);
       client.publish(pub_topic,"ON");
-      Serial.println("/buu/iot/diy/state/fan,ON");
+      Serial.print("publish(");Serial.print(pub_topic);Serial.println(",ON)"); 
     }else if(msgg="OFF_FAN"){
-      data = String("/")+String(phone)+String("/house1/state/fan");
+      data = String("/")+String(phone)+String("/")+String(house)+String("/state/fan");
       data.toCharArray(pub_topic,data.length()+1);
       digitalWrite(FAN,LOW);
       client.publish(pub_topic,"OFF");
-      Serial.println("/buu/iot/diy/state/fan,OFF");
+      Serial.print("publish(");Serial.print(pub_topic);Serial.println(",OFF)"); 
     }
-  }else if(topic==String("/")+String(phone)+String("/house1/control/valve1")){
+  }else if(topic==String("/")+String(phone)+String("/")+String(house)+String("/control/valve1")){
     if(msgg=="ON_VALVE1"){
-      data = String("/")+String(phone)+String("/house1/state/valve1");
+      data = String("/")+String(phone)+String("/")+String(house)+String("/state/valve1");
       data.toCharArray(pub_topic,data.length()+1);
       digitalWrite(VALVE1,HIGH);      
       client.publish(pub_topic,"ON");
+      Serial.print("publish(");Serial.print(pub_topic);Serial.println(",ON)"); 
       
     }else if(msgg="OFF_VALVE1"){
-      data = String("/")+String(phone)+String("/house1/state/valve1");
+      data = String("/")+String(phone)+String("/")+String(house)+String("/state/valve1");
       data.toCharArray(pub_topic,data.length()+1);
       digitalWrite(VALVE1,LOW);
       client.publish(pub_topic,"OFF");
+      Serial.print("publish(");Serial.print(pub_topic);Serial.println(",OFF)"); 
     }
-  }else if(topic==String("/")+String(phone)+String("/house1/control/valve2")){
+  }else if(topic==String("/")+String(phone)+String("/")+String(house)+String("/control/valve2")){
     if(msgg=="ON_VALVE2"){
-      data = String("/")+String(phone)+String("/house1/state/valve2");
+      data = String("/")+String(phone)+String("/")+String(house)+String("/state/valve2");
       data.toCharArray(pub_topic,data.length()+1);
       digitalWrite(VALVE2,HIGH);
       client.publish(pub_topic,"ON");
+      Serial.print("publish(");Serial.print(pub_topic);Serial.println(",ON)"); 
     }else if(msgg="OFF_VALVE2"){
-      data = String("/")+String(phone)+String("/house1/state/valve2");
+      data = String("/")+String(phone)+String("/")+String(house)+String("/state/valve2");
       data.toCharArray(pub_topic,data.length()+1);
       digitalWrite(VALVE2,LOW);
       client.publish(pub_topic,"OFF");
+      Serial.print("publish(");Serial.print(pub_topic);Serial.println(",OFF)"); 
     }
   } 
 }
@@ -131,10 +135,11 @@ void reconnect(){
      if (client.connect(alias, mqttuser, mqttpass )) {      
        char sub_topic[32];
        Serial.println("connected"); 
-       data = String("/")+String(phone)+String("/house1/control/#");
+       data = String("/")+String(phone)+String("/")+String(house)+String("/control/#");
        data.toCharArray(sub_topic,data.length()+1);
 
        client.subscribe(sub_topic);
+       Serial.print("subscribe: ");Serial.println(sub_topic);
      } else {
        Serial.print("failed with state ");
        Serial.print(client.state());
@@ -186,7 +191,7 @@ void loop(){
         temp = dht.readTemperature(); //อ่านค่าอุณหภูมิจาก DHT
 
         if (isnan(humid) || isnan(temp)) { //ตรวจสอบค่าที่อ่านได้ว่าเป็น NaN หรือไม่
-          data = String("/")+String(phone)+String("/house1/state/error");
+          data = String("/")+String(phone)+String("/")+String(phone)+String("/state/error");
           data.toCharArray(pub_topic,data.length()+1);
           Serial.println("Failed to read from DHT sensor!");
 //          client.publish("/buu/iot/diy/state/dht/error","Failed to read from DHT!!!");
@@ -197,29 +202,32 @@ void loop(){
 
           String data = String(humid)+","+String(temp);
           data.toCharArray(str,data.length()+1);
-          data = String("/")+String(phone)+String("/house1/state/dht");
+          data = String("/")+String(phone)+String("/")+String(house)+String("/state/dht");
           data.toCharArray(pub_topic,data.length()+1);
           client.publish(pub_topic,str);
+          Serial.print("publish(");Serial.print(pub_topic);Serial.print(",");Serial.print(str);Serial.println(")");
           
           data = String(temp);
           data.toCharArray(str,data.length()+1);
 //              client.publish("/buu/iot/diy/state/dht",str); //ส่งข้อมูล ความชื้น และ อุณหภูมิ ไปยัง topic ชื่อ /dht
-          data = String("/")+String(phone)+String("/house1/state/temp");
+          data = String("/")+String(phone)+String("/")+String(house)+String("/state/temp");
           data.toCharArray(pub_topic,data.length()+1);
           client.publish(pub_topic,str);
 //          client.publish("/buu/iot/diy/state/temp",str);
+          Serial.print("publish(");Serial.print(pub_topic);Serial.print(",");Serial.print(str);Serial.println(")");
           
           data = String(humid);
           data.toCharArray(str,data.length()+1);
-          data = String("/")+String(phone)+String("/house1/state/humid");
+          data = String("/")+String(phone)+String("/")+String(house)+String("/state/humid");
           data.toCharArray(pub_topic,data.length()+1);
           client.publish(pub_topic,str);
 //          client.publish("/buu/iot/diy/state/humid",str);
+          Serial.print("publish(");Serial.print(pub_topic);Serial.print(",");Serial.print(str);Serial.println(")");
           
-          char s[32];
-          data = "publish(/buu/iot/diy/state/dht,["+String(humid)+","+String(temp)+"]))";
-          data.toCharArray(s,data.length()+1);
-          Serial.println(s);
+//          char s[32];
+//          data = "publish(/buu/iot/diy/state/dht,["+String(humid)+","+String(temp)+"]))";
+//          data.toCharArray(s,data.length()+1);
+//          Serial.println(s);
 
           
        }
@@ -232,25 +240,26 @@ void loop(){
         //moisture_percentage = (100.00 - (moisture * 100.00));
         moisture_percentage = map(moisture,0,1023,0,100);
         if (isnan(moisture_percentage)) { //ตรวจสอบค่าที่อ่านได้ว่าเป็น NaN หรือไม่
-          Serial.println("Failed to read from Moisture sensor!");
-          data = String("/")+String(phone)+String("/house1/state/error");
+//          Serial.println("Failed to read from Moisture sensor!");
+          data = String("/")+String(phone)+String("/")+String(house)+String("/state/error");
           data.toCharArray(pub_topic,data.length()+1);
           client.publish(pub_topic,"Failed to read from SOIL!!!");
 //          client.publish("/buu/iot/diy/state/soil/error","Failed to read from SOIL!!!");
+          Serial.print("publish(");Serial.print(pub_topic);Serial.print(",");Serial.print("Failed to read from SOIL!!!)");
           delay(500);
         }else{
           Serial.print("Moisture value:= ");Serial.println(100-moisture_percentage);
           String data = String(100-moisture_percentage);
           data.toCharArray(str,data.length()+1);
-          data = String("/")+String(phone)+String("/house1/state/moisture");
+          data = String("/")+String(phone)+String("/")+String(house)+String("/state/moisture");
           data.toCharArray(pub_topic,data.length()+1);
           client.publish(pub_topic,str);
 //          client.publish("/buu/iot/diy/state/soil",str); //ส่งข้อมูล ความชื้น และ อุณหภูมิ ไปยัง topic ชื่อ /dht
-
-          char s[32];
-          data = "publish(/buu/iot/diy/state/soil,["+String(100-moisture_percentage)+"]) --> "+String(moisture);
-          data.toCharArray(s,data.length()+1);
-          Serial.println(s);
+          Serial.print("publish(");Serial.print(pub_topic);Serial.print(",");Serial.print(str);Serial.println(")");
+//          char s[32];
+//          data = "publish(/buu/iot/diy/state/soil,["+String(100-moisture_percentage)+"]) --> "+String(moisture);
+//          data.toCharArray(s,data.length()+1);
+//          Serial.println(s);
 
         }
         t_soil = millis();
