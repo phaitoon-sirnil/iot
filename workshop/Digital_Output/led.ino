063be375fd096e0c47f38114713f973d6c5a648a
phaitoon-sirnil <47589376+phaitoon-sirnil@users.noreply.github.com>
2023-02-20 20:38:47 +0700

    Update led.ino
    commit 02

--- workshop/Digital_Output/led.ino
+++ workshop/Digital_Output/led.ino
@@ -1,1 +1,12 @@
 //led.ino
+#define LED D0  // connect D0 pin to LED 
+void setup(){
+  pinMode(LED,OUTPUT); // setup output
+}
+
+void loop(){
+  digitalWrite(LED,HIGH); // Pin D0 is HIGH
+  delay(250);
+  digitalWrite(LED,LOW); // Pin D0 is LOW
+  delay(1000); //delay for one second.
+}
