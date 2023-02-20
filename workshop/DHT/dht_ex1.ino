45a527bfb4fac99baf94ebeb9beab74341ef7256
phaitoon-sirnil <47589376+phaitoon-sirnil@users.noreply.github.com>
2023-02-20 11:49:32 +0700

    Update dht_ex1.ino
    commint #1

--- workshop/DHT/dht_ex1.ino
+++ workshop/DHT/dht_ex1.ino
@@ -1,1 +1,47 @@
 
+// Including the ESP8266 WiFi library
+#include <ESP8266WiFi.h>
+#include "DHT.h"
+
+// Uncomment one of the lines below for whatever DHT sensor type you're using!
+#define DHTTYPE DHT11   // DHT 11
+#define DHTPin D4
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+
+
+// only runs once on boot
+void setup() {
+  // Initializing serial port for debugging purposes
+  Serial.begin(115200);
+  delay(10);
+
+  dht.begin();
+
+  
+}
+
+// runs over and over again
+void loop() {
+  
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+  
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+  
+    }
+    else {
+     
+      Serial.print("Humidity: ");
+      Serial.print(h);
+      Serial.print(" %\t Temperature: ");
+      Serial.print(t);
+      Serial.println(" *C ");     
+    }
+
+    delay(2000);  
+}
